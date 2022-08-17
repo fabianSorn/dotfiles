e41d1c9d5f00fbd7fae4a4310ee7d3a2cb6bc31c
Fabian Sorn <fabian.sorn@icloud.com>
2022-08-17 23:00:40 +0200

    Nicer keybindings for telescope

--- nvim/lua/fs/plugins/setup/telescope.lua
+++ nvim/lua/fs/plugins/setup/telescope.lua
@@ -2,7 +2,11 @@ require("telescope").setup{
   defaults = {
     mappings = {
       i = {
-        ["<C-h>"] = "which_key"
+        ["<C-h>"] = "which_key",
+        ["<C-j>"] = "move_selection_next",
+        ["<C-k>"] = "move_selection_previous",
+        ["<C-p>"] = "cycle_history_prev",
+        ["<C-n>"] = "cycle_history_next",
       }
     }
   },
