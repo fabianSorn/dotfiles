6d66b809fc846efa74f66ca497e32666938966a3
fabian.sorn <fabian.sorn@cas.de>
2022-09-18 21:33:52 +0200

    Add automatic light/dark-mode detection

--- nvim/lua/fs/utils/os.lua
+++ nvim/lua/fs/utils/os.lua
@@ -1,6 +1,47 @@
 local module = { }
 
+function module.in_wsl()
+  if vim.fn.has("unix") then
+    local has_win_mount = string.find(vim.fn.system("echo %$PATH"), "/mnt/c/WINDOWS")
+    if has_win_mount then
+      return true
+    end
+  end
+  return false
+end
+
+function module.in_windows()
+  return vim.fn.has("win64") or vim.fn.has("win32")
+end
+
+function module.in_macos()
+  return vim.fn.has("macunix")
+end
+
+function module.in_linux()
+  return vim.fn.has("unix") and not module.in_wsl()
+end
+
 function module.background()
+  if module.in_wsl() or module.in_windows() then
+      return win_background()
+  elseif module.in_macos() then
+      return mac_background()
+  else
+    return "dark"
+  end
+end
+
+function win_background()
+  local useLightTheme = vim.fn.system("powershell.exe -noprofile -nologo -noninteractive '$a = Get-ItemProperty -Path HKCU:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize; $a.AppsUseLightTheme'")
+  if string.match(useLightTheme, "0") then
+    return "dark"
+  else
+    return "light"
+  end
+end
+
+function mac_background()
   local color = vim.fn.system("defaults read -g AppleInterfaceStyle")
   if string.match(color, ".*Dark.*") then
     return "dark"
